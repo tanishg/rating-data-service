eb0c4a8f6c2d74c0ffd8d29f397edc81d0359549
Tanish Gupta <guptatanish9080@gmail.com>
2021-09-08 19:10:47 +0530

    initial commit

---
+++ src/test/java/io/tainishg/ratingdataservice/RatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tainishg.ratingdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
